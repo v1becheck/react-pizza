392adec1de6f8447b2d1b843421012f99b9a7219
Milos Ilic <milosilic89@outlook.com>
2025-11-08 16:13:37 +0100

    feat: Implemented Routes via React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-function App() {
-  const x = 10;
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './ui/Home';
+import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import Order from './features/order/Order';
+import CreateOrder from './features/order/CreateOrder';
 
-  return <div>Hello World</div>;
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/menu',
+    element: <Menu />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+  {
+    path: '/order',
+    element: <Order />,
+  },
+  {
+    path: '/order/new',
+    element: <CreateOrder />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
