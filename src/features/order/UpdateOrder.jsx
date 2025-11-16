e12fc98b1c2b7f6eebbbb81b8bd9e9ff0d571b8d
Milos Ilic <milosilic89@outlook.com>
2025-11-16 22:59:49 +0100

    feat: Implemented Loader for UpdateOrder

--- src/features/order/UpdateOrder.jsx
+++ src/features/order/UpdateOrder.jsx
@@ -2,17 +2,26 @@ import Button from "../../ui/Button";
 import { useFetcher } from "react-router-dom";
 import { updateOrder } from "../../services/apiRestaurant";
 import { formatCurrency } from "../../utils/helpers";
+import Loader from "../../ui/Loader";
 
 function UpdateOrder({ order }) {
   const fetcher = useFetcher();
   const priorityPrice = order.orderPrice * 0.2;
+  const isUpdating = fetcher.state === "submitting";
 
   return (
-    <fetcher.Form method="PATCH" className="text-right">
-      <Button type="primary" onClick={() => {}}>
-        Make Priority for {formatCurrency(priorityPrice)}
-      </Button>
-    </fetcher.Form>
+    <>
+      {isUpdating && (
+        <div className="fixed inset-0 z-50">
+          <Loader />
+        </div>
+      )}
+      <fetcher.Form method="PATCH" className="text-right">
+        <Button type="primary" disabled={isUpdating} onClick={() => {}}>
+          Make Priority for {formatCurrency(priorityPrice)}
+        </Button>
+      </fetcher.Form>
+    </>
   );
 }
 
